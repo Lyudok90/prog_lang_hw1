3b30c5ec9e7c2e7ede029061e059febf1b548177
Lydok90 <lyudaharutyunyan@gmail.com>
2023-04-13 01:10:59 +0400

    max_min

---
+++ max_min/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1 = new Random().Next(1, 10);
+Console.WriteLine("first number = " + num1);
+
+int num2 = new Random().Next(1, 10);
+Console.WriteLine("second number = " + num2);
+
+if(num1 > num2)
+{
+    Console.WriteLine("max number = " + num1);
+    Console.WriteLine("min number = " + num2);
+}
+else 
+{
+    Console.WriteLine("max number = " + num2);
+    Console.WriteLine("min number = " + num1);
+}
