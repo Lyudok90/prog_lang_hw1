fbbe3bef1587a28e2bbee366f7cce07475ae5008
Lydok90 <lyudaharutyunyan@gmail.com>
2023-04-13 23:19:51 +0400

    max_of_3

---
+++ max_of_3/Program.cs
@@ -0,0 +1,22 @@
+﻿int num1 = new Random().Next(1, 10);
+Console.WriteLine("first number = " + num1);
+
+int num2 = new Random().Next(1, 10);
+Console.WriteLine("second number = " + num2);
+
+int num3 = new Random().Next(1, 10);
+Console.WriteLine("third number = " + num3);
+
+
+if(num1 >= num2 && num1 >= num3) 
+
+Console.WriteLine("max number = " + num1);
+
+else if(num2 >= num1 && num2 >= num3) 
+
+Console.WriteLine("max number = " + num2);
+
+else if(num3 >= num1 && num3 >= num2) 
+
+Console.WriteLine("max number = " + num3);
+  
